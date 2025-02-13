d4dd931b93963ee037a2f23aa70d99ce6e4cf2a1
init-dev-env <init-dev-env-github-id@upost.work>
2025-02-13 19:59:31 +0900

    update

--- __StartExtractionFirst.cmd
+++ __StartExtractionFirst.cmd
@@ -7,7 +7,7 @@ Set CurrentDirPath=%CurrentDirPath:~0,-1%
 PushD "%CurrentDirPath%"
 Net Session 1> Nul 2> Nul
 If ErrorLevel 1 (
-    PowerShell.exe -NoLogo -NoProfile -ExecutionPolicy Bypass -Command "Start-Process -Verb RunAs -ArgumentList \"%CurrentDirPath%\%~nx0\""
+    PowerShell.exe -NoLogo -NoProfile -ExecutionPolicy Bypass -Command "Start-Process -Verb RunAs Cmd.exe -ArgumentList \"/C Call %CurrentDirPath%\%~nx0\""
     GoTo :EOF
 )
 PowerShell.exe -NoLogo -NoProfile -ExecutionPolicy Bypass -Command Invoke-Expression ( ( @(\"\") * 15 ) + ( ( Get-Content \"%~dpnx0\" ) ^| Select-Object -Skip 15 ) -join [char]10 ).Replace(\"{CurrentDirPath}\", \"%CurrentDirPath%\")
@@ -17,6 +17,13 @@ using namespace System.Collections.Generic
 using namespace System.IO
 $ErrorActionPreference = "Stop"
 Push-Location "{CurrentDirPath}"
-$TargetFileName = "init-dev-env.zip"
-Expand-Archive -Path $TargetFileName -DestinationPath "C:\TempInstall"
-Rename-Item C:\TempInstall\init-dev-env C:\TempInstall\InstallScript
+if (Test-Path C:\TempInstall\init-dev-env-main -PathType Container) {
+    Remove-Item C:\TempInstall\init-dev-env-main -Recurse -Force
+}
+Expand-Archive -Path init-dev-env-main.zip -DestinationPath C:\TempInstall -Force
+if (-not (Test-Path C:\TempInstall\InstallScript -PathType Container)) {
+    Rename-Item C:\TempInstall\init-dev-env-main C:\TempInstall\InstallScript -Force
+} else {
+    Move-Item C:\TempInstall\init-dev-env-main\*.* C:\TempInstall\InstallScript -Force
+    Remove-Item C:\TempInstall\init-dev-env-main -Recurse -Force
+}
